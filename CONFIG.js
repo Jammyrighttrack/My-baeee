42464333ffc767f48e6cb3b5fbc5f2a180566693
Jammyrighttrack <jamesclearrighthere@outlook.com>
2026-02-14 10:20:53 +0700

    Modify CONFIGDATA for Valentine's Day
    Updated Valentine's Day message and responses.

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
   titleHeader: "Hi baee, biết là giờ này đã muộn nhưng muốn cho em bất ngờ nhỏ này. </br> Happy Valentine's Day <3",
   descriptionHeader:
-    "Chúc em 1 ngày Valentine thật tuyệt vời và hạnh phúc nhé. Anh yêu em nhiều lắm luôn đó! Còn giờ thì hãy trả lời thật lòng câu này nhé: Em có yêu anh không?",
+    "Chúc em 1 ngày Valentine thật tuyệt vời và hạnh phúc nhé. Yêu em nhiều baee. Còn giờ thì trả lời thật lòng câu này nhé =))): Em có yêu anh không?",
   buttonYes: "Em yêu anh",
   buttonNo: "Cũng thường thôi, ko yêu đâu",
   titleModar: "Cảm ơn em.",
-  descriptionModar:
+  descriptionModar:    
     "Cảm ơn em đã đến bên và ở bên anh. Anh sẽ cố gắng trở thành người bạn trai tuyệt vời và luôn ở bên em cho dù có chuyện gì xảy ra bởi vì anh yêu em nhiều lắmmm!",
 };
